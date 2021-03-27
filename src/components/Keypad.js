cdcde126bb4e8526ad23081d4ce2304ced3c6130
Ian Cossentino <icossentino@gmail.com>
2021-03-27 13:49:38 -0400

    commit tree 8f055f43c86dd84c72d116e6dfcc8b63c0c157e6

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
+import React, { Component } from 'react'
+
+
+
+export default class Keypad extends Component {
+
+  eventHandler = () => console.log("Entering password...")
+  
+  render() {
+    return (
+      <input type="password" onKeyUp={this.eventHandler}></input>
+    )
+  }
+}
+
+
+
+
 // Code Keypad Component Here
